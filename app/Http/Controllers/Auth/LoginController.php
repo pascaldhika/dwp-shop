66fb32e3439335d7412501680201bccdf616cffd
pascaldhika <pascaldhika@gmail.com>
2026-09-18 14:56:54 +0700

    hmh

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,7 +51,7 @@ class LoginController extends Controller
             ]);
         }
 
-        if ($user->hasRole('admin')) {
+        if ($user->hasRole('Super Admin')) {
             return redirect()->intended(RouteServiceProvider::HOME);
         } else{
             return redirect()->intended(RouteServiceProvider::SHOP);
