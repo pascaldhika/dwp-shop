88556f8b759c948490b155823314b0e5afa164db
pascaldhika <pascaldhika@gmail.com>
2026-09-19 10:33:18 +0700

    gfg

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
-        Barryvdh\Snappy\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
